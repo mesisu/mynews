f29722e1debceb8bb2d9759956ac5e1ebb9a04f8
”本村” <amatuki0710@i.softbank.jp>
2020-10-08 05:43:54 +0000

    11-課題4

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,16 +1,16 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
-@extends('layouts.profile')
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
 
 
-{{-- profile.blade.phpの@yield('title')に'プロフィール'を埋め込む --}}
-@section('title', 'プロフィールページ')
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'ニュースの新規作成')
 
-{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール新規作成</h2>
+                <h2>ニュース新規作成</h2>
             </div>
         </div>
     </div>
