7585402f9f0199b85dab44919efae088f1f101ae
”本村” <amatuki0710@i.softbank.jp>
2020-10-27 01:54:39 +0000

    17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -20,27 +20,27 @@ class ProfileController extends Controller
       // Varidationを行う
       $this->validate($request, Profile::$rules);
 
-      $profile = new Profile;
-      $form = $request->all();
+      $profile = new Profile; //使える状態に変更
+      $form = $request->all();//リクエストすべて取得
       
       unset($form['_token']);
       // データベースに保存する
-      $profile->fill($form);
-      $profile->save();
+      $profile->fill($form); //データを整える為fill
+      $profile->save();//登録
         return redirect('admin/profile/create');
     }
 
     public function edit(Request $request)
     {
       // Profile Modelからデータを取得する
-      $profile = Profile::find($request->id);
-      if (empty($profile)) {
+      $profile = Profile::find($request->id); //レコード取得
+      if (empty($profile)) {//空だとえらー
         abort(404);    
       }
         return view('admin.profile.edit', ['profile_form' => $profile]);
     }
 
-    public function update()
+    public function update(Request $request)
     {
         return redirect('admin/profile/edit');
     }
